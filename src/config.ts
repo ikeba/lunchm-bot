92d97cb1adf82e3a79448709c782cf26a4f9b0c7
ikeba <illia.keba@gmail.com>
2026-03-07 15:51:11 +0100

    move excluded categories to env

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,15 @@ const schema = z.object({
   ALLOWED_USER_ID: z.coerce.number(),
   LUNCH_MONEY_BASE_URL: z.string().default('https://api.lunchmoney.dev/v2'),
   LOG_LEVEL: z.enum(['error', 'warn', 'info']).default('info'),
+  EXCLUDED_PAYEES: z
+    .string()
+    .default('[No Payee],Added To EUR,Transfer')
+    .transform(value =>
+      value
+        .split(',')
+        .map(s => s.trim())
+        .filter(Boolean)
+    ),
 })
 
 export const config = schema.parse(Bun.env)
